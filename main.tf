38888e09c82abbca63155637bb80f994a9ad440a
TechlabRS <129305203+TechlabRS@users.noreply.github.com>
2023-07-12 20:03:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameters" {
   name   = var.parameters[count.index].name
   value  = var.parameters[count.index].value
   type   = "String"
-  key_id = "6bde45a8-ffd6-43dd-9674-c52cdbeaa16d"
+  key_id = "1133668a-a5bd-4092-8de8-8155d841df3e"
 }
 
 ## Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
@@ -13,5 +13,5 @@ resource "aws_ssm_parameter" "passwords" {
   name   = var.passwords[count.index].name
   value  = var.passwords[count.index].value
   type   = "SecureString"
-  key_id = "6bde45a8-ffd6-43dd-9674-c52cdbeaa16d"
+  key_id = "1133668a-a5bd-4092-8de8-8155d841df3e"
 }
